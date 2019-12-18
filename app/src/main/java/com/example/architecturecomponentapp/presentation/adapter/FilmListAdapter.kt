b70b1693903628d27aa7322830f96b9d97883170
Ednaldo Martins <martinsednaldo@outlook.com>
2019-12-18 12:38:18 -0300

    Adapter para exibir RecycleView no fragment de lista de filmes

---
+++ app/src/main/java/com/example/architecturecomponentapp/presentation/adapter/FilmListAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.architecturecomponentapp.presentation.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ednaldomartins.architecturecomponentapp.data.entity.Film
+import com.example.architecturecomponentapp.R
+
+class FilmListAdapter (var filmList: List<Film>, context: Context?)
+    : RecyclerView.Adapter<FilmListAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder{
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.adapter_film_list, parent, false)
+
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return filmList.size!!
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val film: Film = filmList.get(position)
+        //falta a capa do filme
+        holder.filmImageView.setImageResource(R.drawable.ic_movie_black_24dp)
+        holder.filmName.text = film.name
+        holder.filmYear.text = film.year.toString()
+    }
+
+        class MyViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+            lateinit var filmImageView: ImageView
+            lateinit var filmName: TextView
+            lateinit var filmYear: TextView
+
+            init {
+                filmImageView = v.findViewById(R.id.adapter_film_list_image_film)
+                filmName = v.findViewById(R.id.adapter_film_list_text_name)
+                filmYear = v.findViewById(R.id.adapter_film_list_text_year)
+            }
+    }
+}
