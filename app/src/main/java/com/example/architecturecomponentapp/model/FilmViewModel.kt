c450520075b7f316b6c25cd5452a7b7320fcd991
Ednaldo Martins <martinsednaldo@outlook.com>
2019-12-20 13:04:46 -0300

    implementação da requisição da API.

--- app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
+++ app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
@@ -2,42 +2,43 @@ package com.example.architecturecomponentapp.model
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.architecturecomponentapp.data.dao.FilmDao
-import com.example.architecturecomponentapp.data.entity.Film
+
 import kotlinx.coroutines.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+import com.example.architecturecomponentapp.data.dao.FilmDao
+import com.example.architecturecomponentapp.data.database.remote.FilmsApi
+import com.example.architecturecomponentapp.data.entity.Film
 
 class FilmViewModel (val databaseDao: FilmDao, app: Application) : AndroidViewModel (app) {
 
     private var viewModelJob = Job()
     private val uiCoroutineScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-    private val films = databaseDao.filmList()
+    private var response = MutableLiveData<Film>()
+    val resp: LiveData<Film> get() = response   //get
 
-    //TEMP
-    var lastFilm = MutableLiveData<Film?>()
+    fun requestFilmApiService () {
+        FilmsApi.filmApiService.callFilmsApi().enqueue( object : Callback<Film> {
+            override fun onFailure(call: Call<Film>, t: Throwable) {
+                response.value = Film(-1,"sem retorno", -1)
+            }
 
-    init {
-        listFilms()
-    }
-
-    // captura ultimo filme chamando funcao de suspencao
-    // DEVE SER ALTERADO PARA MOSTRAR LISTA COMPLETA QUANDO EU FIZER O VIEW
-    private fun listFilms() {
-        uiCoroutineScope.launch { lastFilm.value = getLastFilmFromDatabase() }
-    }
-
-    // funcao de suspensao para retornar ultimo filme inserido
-    private suspend fun getLastFilmFromDatabase(): Film? {
-        return withContext(Dispatchers.IO){ databaseDao.getLastFilm() }
+            override fun onResponse(call: Call<Film>, r: Response<Film>) {
+                if (r.isSuccessful) {
+                    response.value = r.body()
+                    val x = response.value?.year //
+                }
+            }
+        })
     }
 
     // inserir filme chamando funcao de suspensao
     fun insertFilm(name: String, year: Short) {
-        uiCoroutineScope.launch {
-            val film = Film(name = name, year = year)
-            insert(film)
-            lastFilm.value = getLastFilmFromDatabase()
-        }
+        uiCoroutineScope.launch { insert( Film(name = name, year = year) ) }
     }
 
     //funcao de suspensao para inserir film no BD
