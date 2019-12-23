5c6fb231ef66ed9a7b3401f0a68eb717b22a5b21
Ednaldo Martins <martinsednaldo@outlook.com>
2019-12-23 16:11:56 -0300

    Adição do Adapter para usar Coroutines para receber uma lista de Filmes.

--- app/src/main/java/com/example/architecturecomponentapp/data/database/remote/FilmApiService.kt
+++ app/src/main/java/com/example/architecturecomponentapp/data/database/remote/FilmApiService.kt
@@ -1,12 +1,15 @@
 package com.example.architecturecomponentapp.data.database.remote
 
 import com.example.architecturecomponentapp.data.entity.Film
+import com.example.architecturecomponentapp.data.entity.MovieList
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 private val  moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -14,13 +17,25 @@ private val  moshi = Moshi.Builder()
 
 private val  retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(Api.URL)
     .build()
 
 interface FilmApiService {
     // clube da luta (500) nome teste
     @GET("movie/550${Api.UNIQUE_KEY}")
-    fun callFilmsApi(): Call<Film>
+    fun callFilmsApi(): Deferred<Film>  //Defered no lugar de Call para coroutines (para aguardar o resultado sem bloquear outras tarefas)
+
+    // chamar 1 unico filme usando o numero identificador
+    @GET("movie/{film}${Api.UNIQUE_KEY}")
+    fun callFilmApi(@Path("film") film: String): Deferred<Film>
+
+    // chamar lista de filmes mais populares da API
+    @GET("movie/popular${Api.UNIQUE_KEY}")
+    fun callPopularFilmListApi(): Deferred<List<Film>>
+
+    @GET("movie/popular${Api.UNIQUE_KEY}${Api.LANGUAGE}")
+    fun callPopularMovieListApi(): Deferred<MovieList>
 }
 
 object FilmsApi {
--- app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
+++ app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
@@ -6,31 +6,56 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 import kotlinx.coroutines.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 import com.example.architecturecomponentapp.data.dao.FilmDao
+import com.example.architecturecomponentapp.data.database.remote.Api
 import com.example.architecturecomponentapp.data.database.remote.FilmsApi
 import com.example.architecturecomponentapp.data.entity.Film
+import com.example.architecturecomponentapp.data.entity.MovieList
 
 class FilmViewModel (val databaseDao: FilmDao, app: Application) : AndroidViewModel (app) {
 
+    // Threads
     private var viewModelJob = Job()
     private val uiCoroutineScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-    private var response = MutableLiveData<Film>()
-    val resp: LiveData<Film> get() = response
+    // Film
+    private var rFilm = MutableLiveData<Film>()
+    val responseFilm: LiveData<Film> get() = rFilm
+    // Film List
+    //private var rFilmList = MutableLiveData<List<Film>>()
+    //val responseFilmList: LiveData<List<Film>> get() = rFilmList
+//movie
+    private var rFilmList = MutableLiveData<MovieList>()
+    val responseFilmList: LiveData<MovieList> get() = rFilmList
 
     fun requestFilmApiService () {
-        FilmsApi.retrofitService.callFilmsApi().enqueue( object : Callback<Film> {
-            override fun onFailure(call: Call<Film>, t: Throwable) {
-                response.value = Film(-1,"sem retorno", "-1")
+        uiCoroutineScope.launch {
+            //receber a chamada da API sem bloquear a thread princial
+            val getCallDeferred = FilmsApi.retrofitService.callFilmApi("550")//550 = clube da luta
+            try {
+                val listResult = getCallDeferred.await()
+                rFilm.value = listResult
+            }
+            catch (t: Throwable) {
+                rFilm.value = Film(-1,"sem retorno", "-1")
             }
 
-            override fun onResponse(call: Call<Film>, r: Response<Film>) {
-                response.value = r.body()
+        }
+    }
+
+    fun requestFilmListApiService () {
+        uiCoroutineScope.launch {
+            //receber a chamada da API sem bloquear a thread princial
+            val getCallDeferred = FilmsApi.retrofitService.callPopularMovieListApi()
+            try {
+                val listResult = getCallDeferred.await()
+                rFilmList.value = listResult
             }
-        })
+            catch (t: Throwable) {
+                //rFilmList.value = MovieList( LiveData<listOf( Film(title = "Sem retorno: ${t.message}"))> )
+            }
+
+        }
     }
 
     // inserir filme chamando funcao de suspensao
