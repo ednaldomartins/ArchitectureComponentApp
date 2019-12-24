4f036302ea59878b6b51b847af3654293d4fefa0
Ednaldo Martins <martinsednaldo@outlook.com>
2019-12-24 10:51:06 -0300

    Nova Versão do Objeto Film e do DB para pegar mais infomarções da API.

--- app/src/main/java/com/example/architecturecomponentapp/data/database/local/FilmDatabase.kt
+++ app/src/main/java/com/example/architecturecomponentapp/data/database/local/FilmDatabase.kt
@@ -4,17 +4,17 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+
 import com.example.architecturecomponentapp.data.dao.FilmDao
 import com.example.architecturecomponentapp.data.entity.Film
 
-@Database (entities = [Film::class], version = 2, exportSchema = false)
+@Database (entities = [Film::class], version = 3, exportSchema = false)
 abstract class FilmDatabase: RoomDatabase() {
     abstract val filmDao: FilmDao
 
     companion object {
         @Volatile
         private var INSTANCE: FilmDatabase? = null
-
 
         fun getInstance (context: Context): FilmDatabase {
             synchronized(this) {
@@ -27,7 +27,6 @@ abstract class FilmDatabase: RoomDatabase() {
                         .build()
                     INSTANCE = instance
                 }
-
                 return instance
             }
         }
--- app/src/main/java/com/example/architecturecomponentapp/data/entity/Film.kt
+++ app/src/main/java/com/example/architecturecomponentapp/data/entity/Film.kt
@@ -11,13 +11,49 @@ import java.io.Serializable
 data class Film(
     @NonNull @PrimaryKey(autoGenerate = true)
     @Json(name = "id")
-    var id: Long = 0L,
+    var id: Long = -1L,
 
     @ColumnInfo(name = "title")
     @Json(name = "title")
-    var title: String = "",
+    var title: String = "Título do Filme",
 
     @ColumnInfo(name = "release_date")
     @Json(name = "release_date")
-    var releaseData: String = "-1"
+    var releaseData: String = "????-??-??",
+
+    @ColumnInfo(name = "genres")
+    @Json(name = "genres")
+    var genres: Genre?,
+
+    @ColumnInfo(name = "homepage")
+    @Json(name = "homepage")
+    var homepage: String = "www.google.com",
+
+    @ColumnInfo(name = "original_language")
+    @Json(name = "original_language")
+    var originalLanguage: String = "",
+
+    @ColumnInfo(name = "overwiew")
+    @Json(name = "overwiew")
+    var overwiew: String = "",
+
+    @ColumnInfo(name = "popularity")
+    @Json(name = "popularity")
+    var popularity: String = "",
+
+    @ColumnInfo(name = "poster_path")
+    @Json(name = "poster_path")
+    var posterPath: String = "",
+
+    @ColumnInfo(name = "production_company")
+    @Json(name = "production_company")
+    var productionCompany: ProductionCompany?,
+
+    @ColumnInfo(name = "status")
+    @Json(name = "status")
+    var status: String = "",
+
+    @ColumnInfo(name = "vote_average")
+    @Json(name = "vote_average")
+    var voteAverage: Float = .0f
 ) : Serializable
