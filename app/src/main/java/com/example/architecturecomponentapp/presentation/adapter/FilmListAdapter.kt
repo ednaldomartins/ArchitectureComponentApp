9e48b7511454e068e0c67740f96e28a0cfe081aa
Ednaldo Martins <martinsednaldo@outlook.com>
2019-12-26 13:22:15 -0300

    Inserção de imagens na vizualização dos cards.

--- app/src/main/java/com/example/architecturecomponentapp/presentation/adapter/FilmListAdapter.kt
+++ app/src/main/java/com/example/architecturecomponentapp/presentation/adapter/FilmListAdapter.kt
@@ -1,16 +1,19 @@
 package com.example.architecturecomponentapp.presentation.adapter
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.architecturecomponentapp.R
+import com.example.architecturecomponentapp.data.database.remote.Api
 import com.example.architecturecomponentapp.data.entity.Film
 
-class FilmListAdapter (var filmList: List<Film>, context: Context?)
+class FilmListAdapter (private var filmList: List<Film>,private var context: Context?)
     : RecyclerView.Adapter<FilmListAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder{
@@ -25,28 +28,36 @@ class FilmListAdapter (var filmList: List<Film>, context: Context?)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val film: Film = filmList.get(position)
-        //falta a capa do filme
-        //holder.filmPoster.setImageResource(R.drawable.ic_movie_black_24dp)
-        holder.filmTitle.text = film.title
-        holder.filmReleaseDate.text = film.releaseData
+        val film: Film = filmList[position]
+
+        // se tivermos um caminho de uma foto salva, entao...
+        if (film.posterPath != "") {
+            val imgUri = Uri.parse( Api.URL_IMAGE + film.posterPath )
+            Glide.with(holder.filmPoster.context).load(imgUri).into(holder.filmPoster)
+        }
+
+        // nome do filme. alterar para filmes com nome muito grande para nao passar de duas linhas
+        if (film.title.length > 35)
+            holder.filmTitle.text = ("${film.title.subSequence(0,35)}...")
+        else
+            holder.filmTitle.text = film.title
+
+        // alterar o formato da data para dd/mm/aaaa
+        val date = film.releaseData
+        holder.filmReleaseDate.text =
+            ("${date.subSequence(8,10)}/${date.subSequence(5,7)}/${date.subSequence(0,4)}")
+
+        // vizualizacoes do filme na API
         holder.filmPopularity.text = film.popularity
+        // media obtida pela API
         holder.filmVoteAverage.text = film.voteAverage.toString()
     }
 
     class MyViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        var filmPoster: ImageView
-        var filmTitle: TextView
-        var filmReleaseDate: TextView
-        var filmPopularity: TextView
-        var filmVoteAverage: TextView
-
-        init {
-            filmPoster = v.findViewById(R.id.adapter_film_list_image_film)
-            filmTitle = v.findViewById(R.id.adapter_film_list_text_title)
-            filmPopularity = v.findViewById(R.id.adapter_film_list_text_popularity)
-            filmReleaseDate = v.findViewById(R.id.adapter_film_list_text_release_date)
-            filmVoteAverage = v.findViewById(R.id.adapter_film_list_text_vote_average)
-        }
+        var filmPoster: ImageView = v.findViewById(R.id.adapter_film_list_image_film)
+        var filmTitle: TextView = v.findViewById(R.id.adapter_film_list_text_title)
+        var filmReleaseDate: TextView = v.findViewById(R.id.adapter_film_list_text_release_date)
+        var filmPopularity: TextView = v.findViewById(R.id.adapter_film_list_text_popularity)
+        var filmVoteAverage: TextView = v.findViewById(R.id.adapter_film_list_text_vote_average)
     }
 }
