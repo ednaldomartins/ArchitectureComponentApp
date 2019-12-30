8801603517cd646d6a69bc93fdb4e2d33ef00df3
Ednaldo Martins <martinsednaldo@outlook.com>
2019-12-30 18:55:48 -0300

    Classe para adaptar dados dos objetos FilmData e FilmJson.

---
+++ app/src/main/java/com/example/architecturecomponentapp/presentation/adapter/FilmAdapter.kt
@@ -0,0 +1,115 @@
+package com.example.architecturecomponentapp.presentation.adapter
+
+import com.example.architecturecomponentapp.data.entity.FilmData
+import com.example.architecturecomponentapp.model.FilmsJson
+import com.example.architecturecomponentapp.model.Genres
+import com.example.architecturecomponentapp.model.ProductionCompanies
+
+class FilmAdapter {
+    companion object {
+        fun adaptJsonToData (filmJson: FilmsJson.FilmJson) : FilmData{
+            // converter array de genres para string
+            val genres = filmJson.genres?.genreList
+            val genresString = convertGenreArrayToString(genres)
+            // converter array de companies para string
+            val companies = filmJson.productionCompanies?.productionList
+            val companiesString = convertCompanyArrayToString(companies)
+            /*  apos a conversao, criamos o FilmData setando todos os paramentros do FilmJson
+                mais as duas strings convertidas para serem apresentadas na tela de detalhes do filme,
+                e também poder salvar o objeto FilmData no banco de dados Room.
+             */
+            return FilmData(
+                id = filmJson.id,
+                title = filmJson.title,
+                releaseData = filmJson.releaseData,
+                genres = genresString,
+                homepage = filmJson.homepage,
+                originalLanguage = filmJson.originalLanguage,
+                overview = filmJson.overview,
+                popularity = filmJson.popularity,
+                posterPath = filmJson.posterPath,
+                status = filmJson.status,
+                revenue = filmJson.revenue,
+                budget = filmJson.budget,
+                runtime = filmJson.runtime,
+                voteAverage = filmJson.voteAverage,
+                productionCompanies = companiesString
+            )
+        }
+
+        fun adaptDataToJson (filmData: FilmData) : FilmsJson.FilmJson {
+            // converter de string para array de genres
+            val genresString = filmData.genres
+            val genres = convertStringToGenreArray(genresString)
+            // converter de string para array de companies
+            val companiesString = filmData.productionCompanies
+            val companies = convertStringToCompanyArray(companiesString)
+            /*  apos a conversao, criamos o FilmJson setando todos os paramentros do FilmData
+                mais as duas arrays convertidas para serem apresentadas no RecyclerView.
+             */
+            return FilmsJson.FilmJson(
+                id = filmData.id,
+                title = filmData.title,
+                releaseData = filmData.releaseData,
+                genres = genres,
+                homepage = filmData.homepage,
+                originalLanguage = filmData.originalLanguage,
+                overview = filmData.overview,
+                popularity = filmData.popularity,
+                posterPath = filmData.posterPath,
+                status = filmData.status,
+                revenue = filmData.revenue,
+                budget = filmData.budget,
+                runtime = filmData.runtime,
+                voteAverage = filmData.voteAverage,
+                productionCompanies = companies
+            )
+        }
+
+        /**
+         * Metodos para conversao de string para array, e array para string. conversores
+         * para os objetos Genres e ProductionCompanies.
+         */
+        private fun convertGenreArrayToString (genres: Array<Genres.Genre>?): String {
+            val genresSize = genres?.size  ?: 0
+            var genresText = ""
+            for (i in 0 until genresSize)
+                genresText += genres!![i].name + ","
+
+            genresText = genresText.substring(0, genresSize-2) + "."
+            return genresText
+        }
+
+        private fun convertStringToGenreArray (genres: String): Genres {
+            val newGenres = genres.substring(0,genres.length-1)//remover o '.'
+            val arrString = newGenres.split(',')
+            val arrGenres = arrayOf(Genres.Genre())
+            for (i in arrString.indices)
+                arrGenres[i].name = arrString[i]
+
+            return Genres( arrGenres )
+        }
+
+        private fun convertCompanyArrayToString (companies: Array<ProductionCompanies.ProductionCompany>?) : String {
+            val companiesSize = companies?.size  ?: 0
+            var companiesText = ""
+            for (i in 0 until companiesSize)
+                companiesText += companies!![i].name + ", "
+
+            companiesText = companiesText.substring(0, companiesSize-2) + "."
+            return companiesText
+        }
+
+        private fun convertStringToCompanyArray (companies: String): ProductionCompanies {
+            val newCompanies = companies.substring(0,companies.length-1)//remover o '.'
+            val arrString = newCompanies.split(',')
+            val arrCompanies = arrayOf(ProductionCompanies.ProductionCompany())
+            for (i in arrString.indices)
+                arrCompanies[i].name = arrString[i]
+
+            return ProductionCompanies( arrCompanies )
+        }
+
+    }
+
+}
