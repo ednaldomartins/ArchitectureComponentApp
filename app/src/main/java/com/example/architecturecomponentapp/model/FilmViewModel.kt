2dc5887a4e30be950a6a05d42826c6760effe9da
Ednaldo Martins <martinsednaldo@outlook.com>
2020-01-03 19:07:43 -0300

    Manter no tipo Json para ser recuperado pela activity de detalhes.

--- app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
+++ app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.architecturecomponentapp.model
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -13,12 +14,13 @@ import com.example.architecturecomponentapp.data.entity.FilmData
 
 class FilmViewModel (val databaseDao: FilmDao, app: Application) : AndroidViewModel (app) {
 
-    // Threads
+    // Coroutines
     private var viewModelJob = Job()
     private val uiCoroutineScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-    // filme
-    private var rFilm = MutableLiveData<FilmData>()
-    val responseFilmData: LiveData<FilmData> get() = rFilm
+    // dados do filme da API.
+    private var rFilm = MutableLiveData<FilmsJson.FilmJson>()
+    // adaptar resultado para FilmData
+    val responseFilmJson: LiveData<FilmsJson.FilmJson> get() = rFilm
     // lista de filmes da API
     private var rFilmList = MutableLiveData<FilmsJson>()
     val responseFilmList: LiveData<FilmsJson> get() = rFilmList
@@ -27,21 +29,23 @@ class FilmViewModel (val databaseDao: FilmDao, app: Application) : AndroidViewMo
 
     init {
         uiCoroutineScope.launch {
-            val request= FilmsApi.retrofitService.callGenreMovieApi()
-            listGenre = request.await().genreList
+            //val request= FilmsApi.retrofitService.callGenreMovieApi()
+            //listGenre = request.await().genreList
         }
     }
 
-    fun requestFilmApiService () {
+    fun requestFilmApiService (filmId: Long) {
         uiCoroutineScope.launch {
             //receber a chamada da API sem bloquear a thread princial
-            val getCallDeferred = FilmsApi.retrofitService.callFilmApi("550")//550 = clube da luta
+            val getCallDeferred = FilmsApi.retrofitService.callFilmApi(filmId)
             try {
-                val listResult = getCallDeferred.await()
-                rFilm.value = listResult
+                val requestResult = getCallDeferred.await()
+                //atualizando valor do rFilm
+                rFilm.value = requestResult
             }
             catch (t: Throwable) {
-                rFilm.value = FilmData(idDB = -1,title = "sem retorno")
+                val string = t.message
+                Log.e("ERRO - FILM REQUEST", string!!)
             }
         }
     }
@@ -55,14 +59,49 @@ class FilmViewModel (val databaseDao: FilmDao, app: Application) : AndroidViewMo
                 rFilmList.value = listResult
             }
             catch (t: Throwable) {
-                //rFilmList.value = FilmsJson( LiveData<listOf( FilmData(title = "Sem retorno: ${t.message}"))> )
+                val string = t.message
+                Log.e("ERRO - REQUEST", string!!)
             }
         }
     }
 
     // inserir filme chamando funcao de suspensao
-    fun insertFilm(name: String, date: String) {
-        uiCoroutineScope.launch { insert( FilmData(title = name, releaseData = date) ) }
+    fun insertFilm(
+        id: Long,
+        title: String,
+        releaseDate: String,
+        genresString: String,
+        homepage: String,
+        originalLanguage: String,
+        overview: String,
+        popularity: String,
+        posterPath: String,
+        status:String,
+        revenue: Long,
+        budget: Long,
+        runtime: Int,
+        voteAverage: Float,
+        companiesString: String)
+    {
+        uiCoroutineScope.launch {
+            insert( FilmData(
+                id = id,
+                title = title,
+                releaseDate = releaseDate,
+                genres = genresString,
+                homepage = homepage,
+                originalLanguage = originalLanguage,
+                overview = overview,
+                popularity = popularity,
+                posterPath = posterPath,
+                status = status,
+                revenue = revenue,
+                budget = budget,
+                runtime = runtime,
+                voteAverage = voteAverage,
+                productionCompanies = companiesString
+            ) )
+        }
     }
 
     //funcao de suspensao para inserir filmData no BD
