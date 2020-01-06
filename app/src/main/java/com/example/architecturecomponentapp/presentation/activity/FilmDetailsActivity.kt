9cae560cf41f631938c745b3e03d88b3af64e24d
Ednaldo Martins <martinsednaldo@outlook.com>
2020-01-06 02:22:36 -0300

    Adicição de ícone de favorito na tela. para adicionar filme a favorito.

--- app/src/main/java/com/example/architecturecomponentapp/presentation/activity/FilmDetailsActivity.kt
+++ app/src/main/java/com/example/architecturecomponentapp/presentation/activity/FilmDetailsActivity.kt
@@ -3,25 +3,33 @@ package com.example.architecturecomponentapp.presentation.activity
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_film_details.*
 
 import com.example.architecturecomponentapp.R
 import com.example.architecturecomponentapp.data.database.local.FilmDatabase
 import com.example.architecturecomponentapp.data.database.remote.Api
+import com.example.architecturecomponentapp.data.entity.FilmData
 import com.example.architecturecomponentapp.model.FilmViewModel
 import com.example.architecturecomponentapp.model.FilmViewModelFactory
 import com.example.architecturecomponentapp.presentation.adapter.FilmAdapter
-import kotlinx.android.synthetic.main.activity_film_details.*
 
-class FilmDetailsActivity : AppCompatActivity() {
+class FilmDetailsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var favoriteButton: Button
 
     private lateinit var filmViewModel: FilmViewModel
+    private lateinit var film: FilmData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film_details)
+
+        initViews()
         // instanciar viewModel
         initViewModel()
         // recuperar id do filme clicado na ListView
@@ -31,35 +39,33 @@ class FilmDetailsActivity : AppCompatActivity() {
         // quando os dados forem recuperados da API através das co-rotinas apresentar os dados na tela
         filmViewModel.responseFilmJson.observe(this, Observer { json ->
             // aplicar os dados recebido na activity de detalhes
-            //val filmJson = json
-            //filmJson.let { film ->
-            // se tivermos um caminho de uma foto salva, entao...
-
-            val film = FilmAdapter.adaptJsonToData( json )
+            film = FilmAdapter.adaptJsonToData( json )
             if (film.posterPath != "") {
                 val imgUri = Uri.parse( Api.URL_IMAGE + film.posterPath )
                 Glide.with(this.film_details_poster_path.context).load(imgUri).into(this.film_details_poster_path)
             }
 
-            this.film_detail_release_date.text = film.releaseDate
+            film_detail_release_date.text = film.releaseDate
             // adaptar de minutos para horas de filme
-            this.film_detail_runtime.text = ( (film.runtime/60).toString() + "h" + (film.runtime%60).toString() + "m" )
-            this.film_detail_genres.text = film.genres
-            this.film_detail_original_language.text = ( film.originalLanguage.toUpperCase() )
-            this.film_detail_popularity.text = (film.popularity + "  Visualizações.")
-            this.film_detail_average.text = (" ${film.voteAverage} ")
-            //this.film_detail_status.text = film.status
+            film_detail_runtime.text = ( (film.runtime/60).toString() + "h" + (film.runtime%60).toString() + "m" )
+            film_detail_genres.text = film.genres
+            film_detail_original_language.text = ( film.originalLanguage.toUpperCase() )
+            film_detail_popularity.text = (film.popularity + "  Visualizações.")
+            film_detail_average.text = (" ${film.voteAverage} ")
 
-            this.film_details_title.text = film.title
-            this.film_detail_overview.text = film.overview
-            this.film_detail_production_companies.text = film.productionCompanies
-            this.film_detail_budget.text = ("Despesas: " + film.budget + "US$")
-            this.film_detail_revenue.text = ("Receita: " + film.revenue + "US$")
-            this.film_detail_homepage.text = film.homepage
-
-
+            film_details_title.text = film.title
+            film_detail_overview.text = film.overview
+            film_detail_production_companies.text = film.productionCompanies
+            film_detail_budget.text = ("Despesas: " + film.budget + "US$")
+            film_detail_revenue.text = ("Receita: " + film.revenue + "US$")
+            film_detail_homepage.text = film.homepage
         })
 
+    }
+
+    fun initViews() {
+        favoriteButton = film_details_favorite
+        favoriteButton.setOnClickListener(this)
     }
 
     fun initViewModel () {
@@ -70,4 +76,16 @@ class FilmDetailsActivity : AppCompatActivity() {
         filmViewModel = ViewModelProviders.of(this, filmViewModelFactory).get(FilmViewModel::class.java)
     }
 
+    override fun onClick(v: View?) {
+        v?.let {
+            if (v.id == favoriteButton.id) {
+                // se nao for o icone de favorito, entao colocar como favorito e guardar no Database
+                if (favoriteButton.background.state == getDrawable( R.drawable.ic_star_not_favorite_32dp)?.state ) {//usar variavel booleana para fazer essa checagem com seguranca
+                    filmViewModel.insertFilm(film)
+                    favoriteButton.background = getDrawable( R.drawable.ic_star_favorite_32dp)
+                }
+                // senao remover do DB
+            }
+        }
+    }
 }
