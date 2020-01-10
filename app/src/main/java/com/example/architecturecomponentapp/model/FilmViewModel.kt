1093c6ed6cbe49f0978d3c841ee0ed2c1fb8f3e7
Ednaldo Martins <martinsednaldo@outlook.com>
2020-01-10 11:01:48 -0300

    Implementação com novas variáveis e métodos para recuperação e apresentação dos dados do database local.

--- app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
+++ app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModel.kt
@@ -27,8 +27,33 @@ class FilmViewModel (val databaseDao: FilmDao, app: Application) : AndroidViewMo
     val responseFilmList: LiveData<FilmsJson> get() = _requestFilmList
     // lista de generos existentes na API
     private var listGenre: Array<Genres.Genre>? = null
+    /**
+     *  @param _filmsDatabase tem acesso a lista de filmes do database local.
+     *  @param filmsDatabase deve ser usado para qualquer acesso a esse database local de filmes,
+     *  por parte de qualquer fragments ou activities atraves do seu get().
+     *  @param mementoPresentationFilmList foi criado para manter uma copia do estado atual do
+     *  database de filmes. O database local de filmes retona um LiveData da lista de filmes, que so pode
+     *  pode ser acessado atraves do observador. sempre que o observador sinalizar uma modificacao
+     *  nesse database, ele deve repassar o "it" referente a essa lista de filmes, para que o tanto
+     *  o memento, quanto o proprio presentationFilmList sejam atualizados. A principal funcao do
+     *  memento aqui e manter uma copia da lista de filmes do database mesmo apos uma busca, sendo
+     *  assim, caso o presentationFilmList precise reapresentar a lista do database, ele possa fazer
+     *  isso realizando uma copia do memento.
+     *  @param presentationFilmList deve apenas apresentar a lista de filmes do database na tela
+     *  pelo ViewHolder, seja atraves do proprio "it" referente a lista de filmes database, ou
+     *  pela newList criada atraves do filtro de pesquisa, que recebe essa newLista apos ser
+     *  realizado esse filtro nos filmes do database, atraves do mementoPresentationFilmList.
+     */
+    // lista de filmes direto do database
+    private var _filmsDatabase: LiveData<List<FilmData>>? = databaseDao.filmList()
+    // get do database
+    val filmsDataBase: LiveData<List<FilmData>>? get() = _filmsDatabase
+    // mantem o estado atual do database para fazer buscas baseado no database, e nao na lista de apresentacao
+    private var mementoPresentationFilmList: MutableLiveData<List<FilmData>>? = MutableLiveData()
+    // a lista de apresentacao pode ser modificada baseado na busca, o memento so se altera apos alteracao no database
+    var presentationFilmList: MutableLiveData<List<FilmData>>? = MutableLiveData()
 
-    fun requestGenreListApi () {
+    init {
         uiCoroutineScope.launch {
             val request= FilmsApi.retrofitService.callGenreMovieApi()
             listGenre = request.await().genreList
@@ -87,6 +112,33 @@ class FilmViewModel (val databaseDao: FilmDao, app: Application) : AndroidViewMo
                 val string = t.message
                 Log.e("ERRO - REQUEST", string!!)
                 Toast.makeText(getApplication(), "ERRO: não foi possível buscar por filme pesquisado.",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    // armazenar item presentes no database em lista multaveis para apresentacao de dados
+    fun setPresentationDatabase(it: List<FilmData>) {
+        uiCoroutineScope.launch {
+            // memento para backup do estado atual do database e presentation para viewholder
+            mementoPresentationFilmList?.postValue( it )
+            presentationFilmList?.postValue( it )
+        }
+    }
+
+    // buscar filmes no database local
+    fun searchFilmDatabase (query: String) {
+        uiCoroutineScope.launch {
+            // o memento deve ser verificado para ver se esta nulo
+            mementoPresentationFilmList?.value?.let {
+                // caso nao esteja vazio, criaremos um vetor que ira criar uma nova lista de apresentacao
+                val newList: MutableList<FilmData>? = mutableListOf()
+                for (i in 0 until it.size) {
+                    // se contem parte ou toda a palavra deve adicionar a nova lista
+                    if ( it[i].title.toLowerCase().contains(query.toLowerCase()) )
+                        newList?.add(it[i])
+                }
+                // lista de apresentacao recebe a nova lista que foi buscada
+                presentationFilmList?.value = newList
             }
         }
     }
