8ebba11a4ec866666a37421c42156eee5aafacb6
Ednaldo Martins <martinsednaldo@outlook.com>
2020-01-20 21:34:42 -0300

    frabricar FilmDetailsViewModel.

--- app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModelFactory.kt
+++ app/src/main/java/com/example/architecturecomponentapp/model/FilmViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.example.architecturecomponentapp.model
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+
 import com.example.architecturecomponentapp.data.dao.FilmDao
 
 class FilmViewModelFactory (
@@ -16,6 +17,8 @@ class FilmViewModelFactory (
             return FilmApiViewModel(databaseDao, application) as T
         else if (modelClass.isAssignableFrom(FilmDataViewModel::class.java))
             return FilmDataViewModel(databaseDao, application) as T
+        else if (modelClass.isAssignableFrom(FilmDetailsViewModel::class.java))
+            return FilmDetailsViewModel(databaseDao, application) as T
         throw IllegalArgumentException("Classe ViewModel desconhecida")
     }
 
