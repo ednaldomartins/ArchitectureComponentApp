fa92c14e291ec2623c046a6806097d3a730c3d5f
Ednaldo Martins <martinsednaldo@outlook.com>
2020-01-28 02:16:42 -0300

    Remoção de inicialização de Views e listener após tornar a base responsável por elas. Reuso de código.

--- app/src/main/java/com/example/architecturecomponentapp/presentation/fragment/ApiFilmListFragment.kt
+++ app/src/main/java/com/example/architecturecomponentapp/presentation/fragment/ApiFilmListFragment.kt
@@ -2,11 +2,11 @@ package com.example.architecturecomponentapp.presentation.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.*
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 
@@ -16,7 +16,7 @@ import com.example.architecturecomponentapp.presentation.activity.FilmDetailsAct
 import com.example.architecturecomponentapp.presentation.adapter.FilmListAdapter
 import com.example.architecturecomponentapp.util.FilmApiStatus
 
-class ApiFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickListener, View.OnClickListener {
+class ApiFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickListener {
 
     /********************************************************
      *  variaveis herdadas de BaseFilmListFragment:         *
@@ -35,9 +35,10 @@ class ApiFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickLi
         val view = super.onCreateView(inflater, container, savedInstanceState)
         filmViewModel = ViewModelProviders.of(activity!!, super.filmViewModelFactory).get(FilmApiViewModel::class.java)
         initViews(view!!)
+        super.setViewModel(filmViewModel)
 
         // chamar lista de filmes da api
-        filmViewModel.requestFilmListApiService()
+        filmViewModel.setPresentation()
         connectionStatus()
 
         // observador da requiscao
@@ -54,48 +55,43 @@ class ApiFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickLi
         return view
     }
 
+    //  view para imageview que mostra status da conexao
     private fun initViews(v: View) {
-        mStatusImageView = v.findViewById(R.id.film_list_api_status)
-        // inicializacao dos botoes de navegacao de paginas
-        mButtonFirstPage = v.findViewById(R.id.film_list_api_button_first_page)
-        mButtonFirstPage.setOnClickListener(this)
-        mButtonBeforePage = v.findViewById(R.id.film_list_api_button_before_page)
-        mButtonBeforePage.setOnClickListener(this)
-        mButtonNextPage = v.findViewById(R.id.film_list_api_button_next_page)
-        mButtonNextPage.setOnClickListener(this)
-        mButtonLastPage = v.findViewById(R.id.film_list_api_button_last_page)
-        mButtonLastPage.setOnClickListener(this)
-        // localizando o textview do numero da pagina atual
-        mNumberPage = v.findViewById(R.id.film_list_api_number_page)
+        mStatusImageView = v.findViewById(R.id.film_list_status)
     }
 
+    //  atualizar pagina e no super parar animacao do onrefresh
     override fun onRefresh() {
-        filmViewModel.requestFilmListApiService()
+        filmViewModel.setPresentation()
         super.onRefresh()
     }
 
+    //  se o texto for limpado e a query atual nao, entao limpa query e chama pagina de destaques
     override fun onQueryTextChange(newText: String?): Boolean {
         if (newText == "" && filmViewModel.query != "") {
             // limpar consulta
             filmViewModel.setSearch(newText)
-            filmViewModel.requestFilmListApiService(1)
+            filmViewModel.setPresentation(1)
         }
         return true
     }
 
+    //  enviar submissao feita pelo usuario para a pesquisa
     override fun onQueryTextSubmit(query: String?): Boolean {
         filmViewModel.setSearch(query!!)
-        filmViewModel.requestFilmListApiService(1)
+        filmViewModel.setPresentation(1)
         // esconder teclado
         mSearchView.clearFocus()
         return true
     }
 
+    //  ao fechar o menu chamar pagina de destaques
     override fun onOptionsMenuClosed(menu: Menu?) {
-        filmViewModel.requestFilmListApiService(1)
+        filmViewModel.setPresentation(1)
         super.onOptionsMenuClosed(menu)
     }
 
+    //  setar imagem de status da conexao sempre que o estado da conexao mudar
     private fun connectionStatus () {
         filmViewModel.status.observe(this, Observer {
             if (it == FilmApiStatus.LOADING ) {
@@ -118,35 +114,12 @@ class ApiFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickLi
         })
     }
 
-    /**
-     * funcao para controlar o click nos botoes da barra inferior de navegacao de paginas.
-     * Sendo possivel ate o momento passar, voltar, ir para a primeira, e ir para ultima pagina.
-     */
-    override fun onClick(v: View?) {
-        v?.let{
-            when(it.id) {
-                R.id.film_list_api_button_first_page -> {
-                    filmViewModel.requestFilmListApiService(1)
-                }
-                R.id.film_list_api_button_before_page -> {
-                    filmViewModel.requestFilmListApiService( filmViewModel.actualPage - 1 )
-                }
-                R.id.film_list_api_button_next_page -> {
-                    filmViewModel.requestFilmListApiService( filmViewModel.actualPage + 1 )
-                }
-                R.id.film_list_api_button_last_page -> {
-                    filmViewModel.requestFilmListApiService( filmViewModel.totalPages )
-                }
-            }
-        }
-    }
-
+    //  implementacao do click no item da listview
     override fun onFilmClick(filmId: Long?) {
         val intent = Intent(activity, FilmDetailsActivity::class.java)
-        // verificar se esta nos favoritos do database do usuario
         intent.putExtra("filmId", filmId)
-        //  isFavorite = true | false ?
-        val isFavorite = filmViewModel.isFavoriteFilm(filmId!!)
+        // verificar se esta nos favoritos do database do usuario ->  isFavorite = true | false ?
+        val isFavorite: Boolean = filmViewModel.isFavoriteFilm(filmId!!)
         intent.putExtra("favorite", isFavorite)
         startActivity( intent )
     }
--- app/src/main/java/com/example/architecturecomponentapp/presentation/fragment/DataFilmListFragment.kt
+++ app/src/main/java/com/example/architecturecomponentapp/presentation/fragment/DataFilmListFragment.kt
@@ -5,17 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.architecturecomponentapp.R
 
 import com.example.architecturecomponentapp.model.*
 import com.example.architecturecomponentapp.presentation.activity.FilmDetailsActivity
 import com.example.architecturecomponentapp.presentation.adapter.FilmListAdapter
 
-class DataFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickListener, View.OnClickListener {
+class DataFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickListener {
 
     /********************************************************
      *  variaveis herdadas de BaseFilmListFragment:         *
@@ -31,7 +28,7 @@ class DataFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickL
         // recuperar view da super classe
         val view = super.onCreateView(inflater, container, savedInstanceState)
         filmViewModel = ViewModelProviders.of(activity!!, super.filmViewModelFactory).get(FilmDataViewModel::class.java)
-        initViews(view!!)
+        super.setViewModel(filmViewModel)
 
         //  Quando houver alteracao no database, a lista de apresentacao deve ser atualizada
         filmViewModel.filmsDataBase?.observe(this, Observer {
@@ -53,33 +50,21 @@ class DataFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickL
         return view
     }
 
-    private fun initViews(v: View) {
-        // inicializacao dos botoes de navegacao de paginas
-        mButtonFirstPage = v.findViewById(R.id.film_list_api_button_first_page)
-        mButtonFirstPage.setOnClickListener(this)
-        mButtonBeforePage = v.findViewById(R.id.film_list_api_button_before_page)
-        mButtonBeforePage.setOnClickListener(this)
-        mButtonNextPage = v.findViewById(R.id.film_list_api_button_next_page)
-        mButtonNextPage.setOnClickListener(this)
-        mButtonLastPage = v.findViewById(R.id.film_list_api_button_last_page)
-        mButtonLastPage.setOnClickListener(this)
-        // localizando o textview do numero da pagina atual
-        mNumberPage = v.findViewById(R.id.film_list_api_number_page)
-    }
-
+    //  chamar a lista do database padrao e chamar o super para parar a animacao
     override fun onRefresh() {
         filmViewModel.setPresentationDatabase()
         super.onRefresh()   //isRefreshing = false
     }
 
+    //  se o texto da busca for limpo, entao deve recarregar a apresentacao a partir do database
     override fun onQueryTextChange(newText: String?): Boolean {
-        //  se o texto da busca for limpo, entao deve recarregar a apresentacao a partir do database
         if (newText == "")
             filmViewModel.setPresentationDatabase()
 
         return true
     }
 
+    //  enviar query pesquisava pelo usuario para buscar por filme no database que contenha a ela
     override fun onQueryTextSubmit(query: String?): Boolean {
         filmViewModel.searchFilmDatabase(query!!)
         // esconder teclado
@@ -87,30 +72,11 @@ class DataFilmListFragment: BaseFilmListFragment(), FilmListAdapter.OnFilmClickL
         return true
     }
 
-    override fun onClick(v: View?) {
-        v?.let{
-            when(it.id) {
-                R.id.film_list_api_button_first_page -> {
-                    filmViewModel.setPresentation(1)
-                }
-                R.id.film_list_api_button_before_page -> {
-                    filmViewModel.setPresentation( filmViewModel.actualPage - 1 )
-                }
-                R.id.film_list_api_button_next_page -> {
-                    filmViewModel.setPresentation( filmViewModel.actualPage + 1 )
-                }
-                R.id.film_list_api_button_last_page -> {
-                    filmViewModel.setPresentation( filmViewModel.totalPages )
-                }
-            }
-        }
-    }
-
+    //  implementacao do click no item da listview
     override fun onFilmClick(filmId: Long?) {
         val intent = Intent(activity, FilmDetailsActivity::class.java)
         intent.putExtra("filmId", filmId)
         intent.putExtra("favorite", true)
-        // inicia activity com resquestCode = 1 -> abrir a partir do DB
         startActivity( intent )
     }
 
