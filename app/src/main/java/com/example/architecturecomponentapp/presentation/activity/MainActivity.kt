137e98297003d28d12e8654d750f023b47784da1
Ednaldo Martins <martinsednaldo@outlook.com>
2020-01-28 02:14:27 -0300

    Remoção da ViewModel da activity.

--- app/src/main/java/com/example/architecturecomponentapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/architecturecomponentapp/presentation/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.architecturecomponentapp.presentation.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager.widget.ViewPager
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout
@@ -11,25 +10,14 @@ import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
 
 import com.example.architecturecomponentapp.R
-import com.example.architecturecomponentapp.data.database.local.FilmDatabase
-import com.example.architecturecomponentapp.model.FilmViewModel
-import com.example.architecturecomponentapp.model.FilmViewModelFactory
 import com.example.architecturecomponentapp.presentation.fragment.ApiFilmListFragment
 import com.example.architecturecomponentapp.presentation.fragment.DataFilmListFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var filmViewModel: FilmViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // recuperar fonte de dados
-        val application = requireNotNull(this).application
-        val dataSource = FilmDatabase.getInstance(application).filmDao
-        val filmViewModelFactory = FilmViewModelFactory(dataSource, application)
-        filmViewModel = ViewModelProviders.of(this, filmViewModelFactory).get(FilmViewModel::class.java)
 
         val toolbar: Toolbar = findViewById(R.id.main_toolbar)
         toolbar.title = resources.getString(R.string.app_name)
